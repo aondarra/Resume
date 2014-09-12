eb2d62ce4ff309780eda4278f759a54040ce60f9
Aritz Ondarra <aritz.ondarra@gmail.com>
2014-09-12 14:12:21 +0100

    send global

---
+++ send_global.php
@@ -0,0 +1,53 @@
+<?php
+	include("sendConfig.php");
+	$_POST['type'] = (empty($_POST['type']) ? $_GET['a'] : $_POST['type']);
+
+	if(!preg_match('@('.join('|',array_keys($formTypes)).')@',$_POST['type']))
+	{
+		die('Undefined Form Type');
+	}
+	
+	if(empty($_POST['send']))
+	{
+		$temp 			= $_POST;			unset($_POST);
+		$_POST['type']	= $temp['type']; 	unset($temp['type']);
+		$_POST['send'] 	= $temp;			unset($temp);
+	}
+	
+	array_filter($_POST['send'],"clean");
+	
+	//Info Send
+	require_once('classes/class.phpmailer.php');
+	$mail             	= new PHPMailer();
+	$mail->IsSMTP();
+	$mail->SMTPDebug  	= true;
+	$mail->SMTPAuth   	= true;
+	$mail->Host       	= 'smtp.1and1.es'; // ssl://smtp.gmail.com
+	$mail->Port       	= 587; // 465
+	$mail->Username   	= 'EMAIL';
+	$mail->Password   	= 'PASSWORD';
+	$mail->Subject    	= 'New '.$formTypes[$_POST['type']]. ' Message from NAME';
+	$mail->CharSet		= 'utf-8';
+	$mail->WordWrap 	= 50;
+	
+	$mail->SetFrom('EMAIL', 'NAME');
+
+	$mail->AddAddress('DESTINATION');
+	
+	extract($_POST['send']);
+
+	ob_start();
+
+	include(MAIL_TEMPLATE_PATH.$_POST['type'].'.php');
+	
+	$mail->MsgHTML(ob_get_contents());print_r($mail);
+
+	$resp = array();
+  $resp['send'] = $mail->Send();
+  $resp['error'] = $mail->ErrorInfo;
+
+	ob_end_clean();
+	unset($mail);
+	header('content-type:text/html; charset=utf8');
+	exit(json_encode($resp));
+?>
